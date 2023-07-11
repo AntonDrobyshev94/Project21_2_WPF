4a4d052f072d3820a53ab0017d6f21c2d726fa75
Anton Drobyshev <anton.drobyshev94@yandex.ru>
2023-07-11 17:13:24 +0300

    'initial'

---
+++ Project21WPF/ContactDataApi.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Project21WPF
+{
+    public class ContactDataApi
+    {
+        private HttpClient httpClient { get; set; }
+        public ContactDataApi()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public IEnumerable<Contact> GetContacts()
+        {
+            string url = @"https://localhost:7286/api/values";
+
+            string json = httpClient.GetStringAsync(url).Result;
+            return JsonConvert.DeserializeObject<IEnumerable<Contact>>(json);
+        }
+
+        public void AddContacts(Contact contact)
+        {
+            string url = @"https://localhost:7286/api/values";
+
+            var r = httpClient.PostAsync(
+                requestUri: url,
+                content: new StringContent(JsonConvert.SerializeObject(contact), Encoding.UTF8,
+                mediaType: "application/json")
+                ).Result;
+        }
+
+        public void DeleteContact(int id)
+        {
+            string url = $"https://localhost:7286/api/values/{id}";
+            var r = httpClient.DeleteAsync(
+                requestUri: url);
+        }
+
+        public Contact FindContactById(int id)
+        {
+            string url = $"https://localhost:7286/api/values/Details/{id}";
+            string json = httpClient.GetStringAsync(url).Result;
+            return JsonConvert.DeserializeObject<Contact>(json);
+        }
+
+        public void ChangeContact(string name, string surname,
+            string fatherName, string telephoneNumber, string residenceAdress, string description, int id)
+        {
+            Contact contact = new Contact()
+            {
+                Name = name,
+                Surname = surname,
+                FatherName = fatherName,
+                TelephoneNumber = telephoneNumber,
+                ResidenceAdress = residenceAdress,
+                Description = description
+            };
+
+            string url = $"https://localhost:7286/api/values/ChangeContactById/{id}";
+
+            var r = httpClient.PostAsync(
+                requestUri: url,
+                content: new StringContent(JsonConvert.SerializeObject(contact), Encoding.UTF8,
+                mediaType: "application/json")
+                ).Result;
+        }
+    }
+}
---
+++ Project21WPF/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project21WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Contact contactHighlight;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContactDataApi context = new ContactDataApi();
+            try
+            {
+                DataGridView.ItemsSource = context.GetContacts().ToObservableCollection();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            RefreshButton.Click += delegate { DataGridView.ItemsSource = context.GetContacts().ToObservableCollection(); };
+            AddButton.Click += delegate
+            {
+                Contact contact = new Contact()
+                {
+                    Name = "",
+                    Surname = "",
+                    FatherName = "",
+                    TelephoneNumber = "",
+                    ResidenceAdress = "",
+                    Description = ""
+                };
+                AddContactWindow addContactWindow = new AddContactWindow(contact);
+                addContactWindow.ShowDialog();
+
+                if (addContactWindow!=null && addContactWindow.DialogResult.Value)
+                {
+                    context.AddContacts(contact);
+                    DataGridView.ItemsSource = context.GetContacts().ToObservableCollection();
+                    DataGridView.Items.Refresh();
+                }
+            };
+
+            DeleteButton.Click += delegate
+            {
+                if((Contact)DataGridView.SelectedItem != null)
+                {
+                    contactHighlight = (Contact)DataGridView.SelectedItem;
+                    context.DeleteContact(contactHighlight.ID);
+                    DataGridView.ItemsSource = context.GetContacts().ToObservableCollection();
+                    DataGridView.Items.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("Контакт не выбран");
+                }
+                    
+            };
+
+            DetailsButton.Click += delegate
+            {
+                if ((Contact)DataGridView.SelectedItem != null)
+                {
+                    contactHighlight = (Contact)DataGridView.SelectedItem;
+                    Contact contact = new Contact()
+                    {
+                        Name = "",
+                        Surname = "",
+                        FatherName = "",
+                        TelephoneNumber = "",
+                        ResidenceAdress = "",
+                        Description = ""
+                    };
+                    contact = context.FindContactById(contactHighlight.ID);
+                    DetailsWindow detailsWindow = new DetailsWindow(contact);
+                    detailsWindow.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Контакт не выбран");
+                }
+            };
+
+            ContactChangeButton.Click += delegate
+            {
+                if ((Contact)DataGridView.SelectedItem != null)
+                {
+                    contactHighlight = (Contact)DataGridView.SelectedItem;
+                    Contact contact = new Contact()
+                    {
+                        Name = contactHighlight.Name,
+                        Surname = contactHighlight.Surname,
+                        FatherName = contactHighlight.FatherName,
+                        TelephoneNumber = contactHighlight.TelephoneNumber,
+                        ResidenceAdress = contactHighlight.ResidenceAdress,
+                        Description = contactHighlight.Description
+                    };
+                    try
+                    {
+                        ChangeContactWindow changeContactWindow = new ChangeContactWindow(contact);
+                        changeContactWindow.ShowDialog();
+                        if (changeContactWindow.DialogResult.Value)
+                        {
+                            context.ChangeContact(contact.Name, contact.Surname,
+                                contact.FatherName, contact.TelephoneNumber,
+                                contact.ResidenceAdress, contact.Description,
+                                contactHighlight.ID);
+                            DataGridView.ItemsSource = context.GetContacts().ToObservableCollection();
+                            DataGridView.Items.Refresh();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Контакт не выбран");
+                }
+            };
+        }
+    }
+}
